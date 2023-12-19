a07ebf2f7f2c3a9d1c556c282bf0e904c2b60ab1
eldstal <laeder.keps@gmail.com>
2023-12-19 22:42:07 +0100

    TURBO IO 2.0

--- _ärt/io.s
+++ _ärt/io.s
@@ -11,6 +11,18 @@ newline:
 		db 0xa, 0
 
 
+%define STDIN_BUF_SIZE 4096
+
+stdin_buf:
+    times STDIN_BUF_SIZE db 0
+
+stdin_buf_length:
+    dq 0    ; total number of bytes placed in stdin_buf
+            ; If < STDIN_BUF_SIZE, it's EOF
+
+stdin_buf_idx:
+    dq 0    ; inside of std_buf, next byte to read
+
     SECTION .text
 ;
 ; rcx: string buffer
@@ -69,7 +81,7 @@ print_int:
 ;
 ; Reads one whole line, replacing the \n with a NULL
 ; Blocking!
-; rcx: file descriptor (0, probably)
+; rcx: file descriptor (ignored)
 ; rdx: output buffer
 ; r8: output buffer size
 ; returns number of read bytes, or -1 on buffer overrun
@@ -80,16 +92,39 @@ read_line:
   push rbx
   push rcx
   push rdx
+  push r14
   push r15
 
   xor rbx, rbx    ; Total read count
-  mov r15, rcx
+  ;mov r15, rcx
 
   read_line_loop:
 
-    syscall3 SYS_read, r15, rdx, 1
-    cmp rax, 1
-    jl read_line_done   ; EOF or error
+
+    mov r15, [stdin_buf_idx]
+    mov r14, [stdin_buf_length]
+    cmp r15, r14
+    jne read_line_buf_filled  ; No need to fill the buffer just yet
+
+    read_line_refill:
+      xor rax,rax
+      mov [stdin_buf_idx], rax
+      syscall3 SYS_read, 0, stdin_buf, STDIN_BUF_SIZE
+      mov [stdin_buf_length], rax
+      cmp rax, 1
+      jl read_line_done   ; EOF or error
+
+
+    read_line_buf_filled:
+
+    ; Read one byte from the buffer to [rdx]
+    mov rdi, stdin_buf
+    add rdi, [stdin_buf_idx]
+    mov rdi, [rdi]
+    mov [rdx], rdi
+
+    inc qword [stdin_buf_idx]
+
 
     cmp [rdx], byte 0xa
     je read_line_done
@@ -114,6 +149,7 @@ read_line:
   mov rax, rbx    ; return value
 
   pop r15
+  pop r14
   pop rdx
   pop rcx
   pop rbx
